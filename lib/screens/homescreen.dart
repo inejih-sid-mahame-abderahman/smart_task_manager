25b9a9c11a9f71258b23042779b23cbf6215a096
inejih sid mhamed abderahman <inejihsidmhamed801@gmail.com>
2026-02-28 23:47:54 +0000

    Implement Homescreen layout and add HomescreenProvider class

--- lib/screens/homescreen.dart
+++ lib/screens/homescreen.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class Homescreen extends StatelessWidget {
   const Homescreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          leading: Icon(Icons.menu, size: 30),
+          title: Text(
+            "my tasks",
+            style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+          ),
+          centerTitle: true,
+          actions: [Icon(Icons.add, size: 50, color: Colors.blueAccent)],
+        ),
+      ),
+    );
   }
 }
